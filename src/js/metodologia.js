8e7185ca7a426cefd19705cc4954a2fa06fbf286
Geraldine_Molina <gerald0805xd@gmail.com>
2021-07-01 11:09:09 -0500

    primer modal

--- src/js/metodologia.js
+++ src/js/metodologia.js
@@ -1,11 +1,46 @@
 
 /* ..........modales ........*/ 
-function modalAries(){
-	document.getElementById("modalAries").style.display="block"; // Esto muestra la informacion 
+
+/*modal1*/
+function modalChildrens(){
+	document.getElementById("modalNiños").style.display="block"; // Esto muestra la informacion 
 
 	var cerrar = document.getElementsByClassName("close")[0];
 
 	cerrar.onclick = function() {
-		document.getElementById("modalAries").style.display= "none";//Esto es para que aparezca la X para cerrar
+		document.getElementById("modalNiños").style.display= "none";//Esto es para que aparezca la X para cerrar
+	}
+}
+
+/*modal2*/
+function modalAdults(){
+	document.getElementById("ModalAdulto").style.display="block"; // Esto muestra la informacion 
+
+	var cerrar = document.getElementsByClassName("close")[0];
+
+	cerrar.onclick = function() {
+		document.getElementById("ModalAdulto").style.display= "none";//Esto es para que aparezca la X para cerrar
+	}
+}
+
+/*modal3*/
+function modalInstitutions(){
+	document.getElementById("container_insti").style.display="block"; // Esto muestra la informacion 
+
+	var cerrar = document.getElementsByClassName("close")[0];
+
+	cerrar.onclick = function() {
+		document.getElementById("container_insti").style.display= "none";//Esto es para que aparezca la X para cerrar
+	}
+}
+
+/*modal4*/
+function modalBussines(){
+	document.getElementById("container_empresa").style.display="block"; // Esto muestra la informacion 
+
+	var cerrar = document.getElementsByClassName("close")[0];
+
+	cerrar.onclick = function() {
+		document.getElementById("container_empresa").style.display= "none";//Esto es para que aparezca la X para cerrar
 	}
 }
